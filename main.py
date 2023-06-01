204d1caa8c24053907f5978b1a63d459f3235c47
nik <72725100+nikiopta@users.noreply.github.com>
2023-06-01 18:00:19 +0300

    adding main.py/the ai model

---
+++ main.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+tf.keras.datasets.mnist.load_data(path="mnist.npz")
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
+model = keras.Sequential([
+    keras.Input((28, 28, 1)),
+    layers.Conv2D(16, 3, activation="relu"),
+    layers.Conv2D(32, 3, activation="relu"),
+    layers.MaxPooling2D(),
+    layers.Flatten(),
+    layers.Dense(10, activation="softmax")
+])
+
+model.compile(
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=keras.optimizers.Adam(learning_rate=0.01),
+    metrics=["accuracy"]
+)
+model = keras.models.load_model("saved_model")
+print(model.summary())
+model.fit(x_train, y_train, batch_size=64, epochs=25, callbacks=[keras.callbacks.ModelCheckpoint("saved_model")],)
+model.evaluate(x_test, y_test)
