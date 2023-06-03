3090ba05dbdd23f353cceaf4a2bc1696a93cbc81
nikiopta <cxghj55@gmail.com>
2023-06-03 12:53:55 +0300

    bug fix

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import numpy as np
 # Load the saved AI model
 model = tf.keras.models.load_model('saved_model')
 
-
 # Function to handle image upload
 def upload_image():
     file_path = filedialog.askopenfilename()
@@ -18,7 +17,7 @@ def upload_image():
         processed_image = preprocess_image(image)
 
         # Convert the processed image to a format compatible with the model
-        input_data = tf.expand_dims(processed_image, axis=0)
+        input_data = np.expand_dims(processed_image, axis=0)
 
         # Make predictions using the model
         predictions = model.predict(input_data)
@@ -41,12 +40,14 @@ def preprocess_image(image):
     processed_image = np.array(processed_image)
     # Reshape the image to add the channel dimension
     processed_image = processed_image.reshape((28, 28, 1))
+    # Normalize the image
+    processed_image = processed_image / 255.0
     return processed_image
 
 # Function to get the predicted class
 def get_predicted_class(predictions):
     # Get the predicted class label from the predictions
-    predicted_class = tf.argmax(predictions, axis=1).numpy()[0]
+    predicted_class = np.argmax(predictions, axis=1)[0]
     return predicted_class
 
 # Create the main application window
